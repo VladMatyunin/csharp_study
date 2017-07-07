ce58f2432b3247d073222fd510f11e8f3c05d5f6
vlad-matyunin96 <vlad.matyunin96@gmail.com>
2017-07-08 02:46:20 +0300

    Add project files.

---
+++ HelloWorld/Models/bookStore/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelloWorld.Models.bookStore
+{
+    public class Book
+    {
+
+        public int Id { get; set; }
+
+        public string name { get; set; }
+
+        public string author { get; set; }
+
+        public int price { get; set; }
+    }
+}
---
+++ HelloWorld/Models/bookStore/Purchase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelloWorld.Models.bookStore
+{
+    public class Purchase
+    {
+        public int PurchaseId { get; set; }
+
+        public string person { get; set; }
+
+        public string address { get; set; }
+
+        public int BookId { get; set; }
+
+        public DateTime datePurchased{get; set;}
+    }
+}

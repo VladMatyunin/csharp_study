198d605ef6b26fc83f28333e4e01ec6e5bfd84ae
vlad-matyunin96 <vlad.matyunin96@gmail.com>
2017-07-29 18:31:02 +0300

    simple muituthreading with ThreadPool

---
+++ Async_MultiThreading/SimpleAsync/Program.cs
@@ -0,0 +1,57 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Starter
+{
+    class Program
+    {
+        public static void Main2()
+        {
+            //returnung Current main ThreadId
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            //Executing method, passing as a parameter
+            //Easiest way to set multithreading
+            ThreadPool.QueueUserWorkItem(method => {
+                int x = Thread.CurrentThread.ManagedThreadId;
+                for (int i = 0; i < 100; i++)
+                {
+                    Console.WriteLine("THREAD IS {0}, COUNT IS {1}", x, i);
+                }
+                
+            });
+            ThreadPool.QueueUserWorkItem(Compute, "HELLO FROM COMPUTE METHOD");
+            //pass message to another Thread 
+            CallContext.LogicalSetData("name", "Vlad");
+            ThreadPool.QueueUserWorkItem(ComputeWithMessaging);
+
+            //Restrict context copying, so "name" will not be seen
+            //This fasters server-like apps
+            ExecutionContext.SuppressFlow();
+
+            ThreadPool.QueueUserWorkItem(ComputeWithMessaging);
+
+            ExecutionContext.RestoreFlow();
+            Console.ReadLine();
+        }
+
+        internal static void Compute(Object state)
+        {
+            Thread.Sleep(2);
+            Console.WriteLine("THREAD IS {0}, MESSAGE IS :{1}",Thread.CurrentThread.ManagedThreadId,state);
+        }
+
+        //Gets message from context
+        static void ComputeWithMessaging(Object state)
+        {
+            Thread.Sleep(3);
+            string message = (string) CallContext.LogicalGetData("name");
+            Console.WriteLine("GOT PASSED MESSAGE: " + message);
+        }
+    }
+}
---
+++ Async_MultiThreading/SimpleAsync/ProgramWithCancel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Starter
+{
+    class ProgramWithCancel
+    {
+        public static void Main()
+        {
+            //register cancellationToken
+            CancellationTokenSource tokenSource = new CancellationTokenSource();
+
+            //pass it as method parameter
+            ThreadPool.QueueUserWorkItem(method=>ProcessWithCancellation(tokenSource.Token, "MESSAGE IS TO CONTINUE"));
+            Console.WriteLine("PRESS ANY KEY TO STOP ANOTHER PROCESS");
+            Console.ReadLine();
+            //stop process
+            tokenSource.Cancel();
+
+            Console.WriteLine("STOPPED");
+            Console.ReadLine();
+        }
+
+        //writing message until not stopped
+        private static void ProcessWithCancellation(CancellationToken cancelToken, string message)
+        {
+            for(int i = 0; i < 100; i++)
+            {
+                //check if cancel is requested
+                if (cancelToken.IsCancellationRequested)
+                    break;
+                Thread.Sleep(100);
+                Console.WriteLine(message);
+            }
+        }
+        private static void TestWithRegister()
+        {
+            Action func = ()=> {
+                Console.WriteLine("GOT IN THE METHOD");
+            };
+            var cts1 = new CancellationTokenSource();
+            cts1.Token.Register(func);
+        }
+    }
+}
